3b57668503548db2a1a28f84d4c76bc017f61a82
Henry Wallis <henry.wallis@unmind.com>
2024-09-10 12:00:49 +0100

    chore: dependabot demo init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/henry-wallis-unmind/dependabot-test
+
+go 1.23.0
+
+require (
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"go.uber.org/zap"
+	"log"
+)
+
+func main() {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Sync()
+	sugar := logger.Sugar()
+	sugar.Info("Hello")
+}
